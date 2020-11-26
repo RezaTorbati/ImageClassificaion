f4334227a13539ccf416fd052f1c241053696705
RezaTorbati <kofcrotmgiv@gmail.com>
2020-11-26 12:13:45 -0600

    Converted the cifar test batches to a gray scale csv and uploading code I used to do it with

---
+++ CifarToBWCsv.py
@@ -0,0 +1,27 @@
+import pickle
+import numpy as np
+import pandas as pd
+
+directory = "cifar-10-batches-py"
+dict = 0
+files = ["data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5", "test_batch"]
+
+for f in files:
+    file = directory + "/" + f
+    with open(file, "rb") as fo:
+        dict = pickle.load(fo, encoding='bytes')
+
+    labels = dict[b'labels']
+    data = dict[b'data']
+
+    bw = np.ones((10000, 1024))
+    for i in range(0,len(data)):
+        dataLength = int(len(data[i]) / 3)
+        for j in range(0,dataLength):
+            bw[i, j] = round((data[i, j] + data[i, j + dataLength] + data[i, j + dataLength * 2]) / 3) #gets the black and white value
+
+    df = pd.DataFrame(data=bw)
+    df['label'] = labels
+
+    writeName = file + ".csv"
+    df.to_csv(writeName, index = False)
